af399df87b58aa365ec96f915f3813a62b9088ac
bomblim <bomblim@naver.com>
2019-09-21 16:03:08 +0900

    first commit (spring boot initial)

---
+++ src/test/java/com/bomblim/excelclassifier/ExcelclassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomblim.excelclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelclassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
