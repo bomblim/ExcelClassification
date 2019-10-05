830d5163545644cf4c5e61f8cf77e33d398ba070
bomblim <bomblim@naver.com>
2019-10-05 15:52:36 +0900

    environment

---
+++ src/main/java/com/dennis/xlsxextract/XlsxextractApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.xlsxextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsxextractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsxextractApplication.class, args);
+	}
+
+}
